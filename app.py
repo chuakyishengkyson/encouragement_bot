6cb76e9cf36c1ac5630fd2ee2be580f27c8a4a7e
chuakyishengkyson <52947000+chuakyishengkyson@users.noreply.github.com>
2020-10-19 14:23:12 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,51 @@
+print("Title of program: Post Exam Activity Bot")
+print()
+while True:
+  description = input("Could you describe how you feel in a sentence?")
+
+  list_of_words = description.split()
+
+  feelings_list = []
+  encouragement_list = []
+  counter = 0
+  
+  for each_word in list_of_words:
+    
+    if each_word == "bored":
+      feelings_list.append("bored")
+      encouragement_list.append("to do something that you like")
+      counter += 1
+    if each_word == "confused":
+      feelings_list.append("confused")
+      encouragement_list.append("to keep revising")
+      counter += 1
+    if each_word == "tired":
+      feelings_list.append("tired")
+      encouragement_list.append("you are stronger than you think")
+      counter += 1
+
+  if counter == 0:
+    
+      output = "Sorry I don't really understand. Please use different words?"
+
+  elif counter == 1:
+    
+      output = "It seems that you are feeling quite " + feelings_list[0] + ". However, do remember that "+ encouragement_list[0] + "! Hope you feel better :)"  
+
+  else:
+
+    feelings = ""    
+    for i in range(len(feelings_list)-1):
+      feelings += feelings_list[i] + ", "
+    feelings += "and " + feelings_list[-1]
+    
+    encouragement = ""    
+    for j in range(len(encouragement_list)-1):
+      encouragement += encouragement_list[i] + ", "
+    encouragement += "and " + encouragement_list[-1]
+
+    output = "It seems that you are feeling quite " + feelings + ". Please always remember "+ encouragement + "! Hope you feel better :)"
+
+  print()
+  print(output)
+  print()
